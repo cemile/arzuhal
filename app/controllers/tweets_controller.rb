ed45459ece2bef7b6683bbff3e3d1700c609de41
Cemile Sarıcaoğlu <saricaoglucemile@gmail.com>
2014-08-21 01:37:39 +0300

    sayfalar oluşturuldu

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,4 +7,22 @@ class TweetsController < ApplicationController
   def show
     @tweet = Tweet.find(params[:id])
   end
+
+  def create
+    @user = User.find(params[:user_id])
+    @tweet = @user.tweets.create(tweet_params)
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @tweet = @user.tweets.find(params[:id])
+    @tweet.destroy
+    redirect_to user_path(@user)
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:text)
+  end
 end
