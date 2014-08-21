82933460ac7c3215b2ef96dc7008e3d6ac07b50b
Cemile Sarıcaoğlu <saricaoglucemile@gmail.com>
2014-08-21 11:32:35 +0300

    validation kısmına gelindi

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,9 +9,15 @@ class TweetsController < ApplicationController
   end
 
   def create
+
     @user = User.find(params[:user_id])
-    @tweet = @user.tweets.create(tweet_params)
-    redirect_to user_path(@user)
+    @tweet = @user.tweets.new(tweet_params)
+    if @tweet.save
+      redirect_to user_path(@user)
+    else
+      render 'users/show'
+    end
+
   end
 
   def destroy
@@ -21,7 +27,9 @@ class TweetsController < ApplicationController
     redirect_to user_path(@user)
   end
 
+
   private
+
   def tweet_params
     params.require(:tweet).permit(:text)
   end
