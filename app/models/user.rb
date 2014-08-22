502f6b4364ea962974183c7b1f3ec7b830fd2afa
Cemile Sarıcaoğlu <saricaoglucemile@gmail.com>
2014-08-22 15:57:47 +0300

    validateler tamamlandı

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
   has_many :tweets
 
+  validates :name, presence: true
+  validates :surname, presence: true
+  validates :email, presence: true
+  validates :bio, presence: true
+
 end
